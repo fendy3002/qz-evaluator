e7eb54a4960bcbe661100677f842d1cf4b1da0ee
Fendy Heryanto <fendy.heryanto@valista-indonesia.com>
2021-03-30 14:49:02 +0700

    dev

--- frontend/src/parse/index.tsx
+++ frontend/src/parse/index.tsx
@@ -4,74 +4,73 @@ let parse = (workspace, objSource) => {
 
     let evaluatorBlock = workspace.newBlock('evaluator');
     evaluatorBlock.initSvg();
-    parser(workspace).obj(evaluatorBlock.getInput("json"), objSource);
+    parser(workspace).obj(evaluatorBlock.getInput("json").connection, objSource);
     workspace.render();
 };
 let parser = (workspace) => {
-    let obj = (parentConnector, objSource) => {
+    let obj = (parentConnection, objSource) => {
         for (let key of Object.keys(objSource)) {
             let blockType = key.substring(1);
-            blockLogic[blockType]?.(parentConnector, objSource);
+            blockLogic[blockType]?.(parentConnection, objSource);
         }
     };
 
-    let string = (parentConnector, objSource) => {
+    let string = (parentConnection, objSource) => {
         let newBlock = workspace.newBlock('string', true);
         newBlock.initSvg();
 
         newBlock.setFieldValue(objSource["$string"], "string_value");
         let newBlockOutput = newBlock.outputConnection;
-        newBlockOutput.connect(parentConnector);
+        newBlockOutput.connect(parentConnection);
     };
-    let number = (parentConnector, objSource) => {
+    let number = (parentConnection, objSource) => {
         let newBlock = workspace.newBlock('number', true);
         newBlock.initSvg();
 
         newBlock.setFieldValue(objSource["$number"], "number_value");
         let newBlockOutput = newBlock.outputConnection;
-        newBlockOutput.connect(parentConnector);
+        newBlockOutput.connect(parentConnection);
     };
-    let boolean = (parentConnector, objSource) => {
+    let boolean = (parentConnection, objSource) => {
         let newBlock = workspace.newBlock('boolean', true);
         newBlock.initSvg();
 
         let booleanValue = objSource["$boolean"];
         newBlock.setFieldValue(booleanValue ? "TRUE" : "FALSE", "bool_value");
         let newBlockOutput = newBlock.outputConnection;
-        newBlockOutput.connect(parentConnector);
+        newBlockOutput.connect(parentConnection);
     };
-    let prop_string = (parentConnector, objSource) => {
+    let prop_string = (parentConnection, objSource) => {
         let newBlock = workspace.newBlock('prop_string', true);
         newBlock.initSvg();
 
         newBlock.setFieldValue(objSource["$prop_string"], "prop_name");
         let newBlockOutput = newBlock.outputConnection;
-        newBlockOutput.connect(parentConnector);
+        newBlockOutput.connect(parentConnection);
     };
-    let prop_number = (parentConnector, objSource) => {
+    let prop_number = (parentConnection, objSource) => {
         let newBlock = workspace.newBlock('prop_number', true);
         newBlock.initSvg();
 
         newBlock.setFieldValue(objSource["$prop_number"], "prop_name");
         let newBlockOutput = newBlock.outputConnection;
-        console.log("parentConnector", parentConnector);
-        newBlockOutput.connect(parentConnector);
+        newBlockOutput.connect(parentConnection);
     };
-    let prop_boolean = (parentConnector, objSource) => {
+    let prop_boolean = (parentConnection, objSource) => {
         let newBlock = workspace.newBlock('prop_boolean', true);
         newBlock.initSvg();
 
         newBlock.setFieldValue(objSource["$prop_boolean"], "prop_name");
         let newBlockOutput = newBlock.outputConnection;
-        newBlockOutput.connect(parentConnector);
+        newBlockOutput.connect(parentConnection);
     };
-    let prop_date = (parentConnector, objSource) => {
+    let prop_date = (parentConnection, objSource) => {
         let newBlock = workspace.newBlock('prop_date', true);
         newBlock.initSvg();
 
         newBlock.setFieldValue(objSource["$prop_date"], "prop_name");
         let newBlockOutput = newBlock.outputConnection;
-        newBlockOutput.connect(parentConnector);
+        newBlockOutput.connect(parentConnection);
     };
 
     let blockLogic = {
