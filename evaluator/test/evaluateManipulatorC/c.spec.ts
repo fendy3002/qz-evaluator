33cfc7ee726b3fd406cb5f006db1c34585f7d13a
Fendy Heryanto <fendy.heryanto@valista-indonesia.com>
2021-04-01 09:13:30 +0700

    dev

--- evaluator/test/evaluateManipulatorC/c.spec.ts
+++ evaluator/test/evaluateManipulatorC/c.spec.ts
@@ -22,4 +22,24 @@ mocha.describe('Evaluate Manipulator C', function () {
         assert.strictEqual(50, result);
         assert.notStrictEqual("50", result);
     });
+    mocha.it('should evaluate c_string_todate', async function () {
+        let result = evaluateJSON.evaluate({}, {
+            $c_string_todate: {
+                source: { $string: "2020-01-01" },
+                format_from: "YYYY-MM-DD"
+            }
+        });
+        assert.equal(true, result instanceof Date);
+        assert.equal("2020-01-01", moment(result).format("YYYY-MM-DD"));
+    });
+    mocha.it('should evaluate c_dateto_string', async function () {
+        let result = evaluateJSON.evaluate({}, {
+            $c_dateto_string: {
+                source: { $date: "2020-01-01" },
+                format_to: "YYYY-MM-DD"
+            }
+        });
+        assert.equal("2020-01-01", result);
+        assert.equal("string", typeof result);
+    });
 });
