595fcea7e060191702846d48d839b219a3a83067
Fendy Heryanto <fendy.heryanto@valista-indonesia.com>
2021-04-01 16:41:31 +0700

    parse json

--- evaluator/test/evaluateManipulatorM/m_sum.spec.ts
+++ evaluator/test/evaluateManipulatorM/m_sum.spec.ts
@@ -6,10 +6,10 @@ let moment = require('moment');
 mocha.describe('Evaluate Manipulator Sum', function () {
     mocha.it('should evaluate sum', async function () {
         let result = evaluateJSON.evaluate({}, {
-            $m_sum: [
-                { $number: 50 },
-                { $number: 30 },
-                { $number: 72 }
+            "$m_sum": [
+                { "$number": 50 },
+                { "$number": 30 },
+                { "$number": 72 }
             ]
         });
         assert.equal(152, result);
